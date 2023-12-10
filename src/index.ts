9d112ca74a7f33e3c89c76f77eae7739c2eda120
k-natori1025 <k-natori1025@gmail.com>
2023-12-10 20:48:03 +0900

    型エイリアスでオブジェクトの型定義

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,21 @@
 // unknownSample()
 
 // 関数の型定義
-import { logMessage } from "./function/basic";
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from "./function/parameters";
+// import { logMessage } from "./function/basic";
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from "./function/parameters";
 
-logMessage("Hello TypeScript")
-isUserSignedIn("ABC", 'Torahack')
-isUserSignedIn("DEF")
-isUserSignedIn2("ABC")
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log("Function parameters sample 5:", sum)
+// logMessage("Hello TypeScript")
+// isUserSignedIn("ABC", 'Torahack')
+// isUserSignedIn("DEF")
+// isUserSignedIn2("ABC")
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log("Function parameters sample 5:", sum)
+
+//  オブジェクトの型定義
+import objectSample from "./object/object";
+import typeAiliasSample from "./object/alias";
+import { type } from "os";
+
+objectSample()
+typeAiliasSample()
+
